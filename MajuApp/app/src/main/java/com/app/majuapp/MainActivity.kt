50bab2eb27bead38db72c20d4068a001ef21c38d
chang0_0 <bigyoung8375@naver.com>
2024-06-14 16:09:55 +0900

    git ignore add"

--- MajuApp/app/src/main/java/com/app/majuapp/MainActivity.kt
+++ MajuApp/app/src/main/java/com/app/majuapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
@@ -23,8 +22,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             MajuAppTheme {
                 StatusBarColorChange()
-                 val navController = rememberNavController()
-                 SetUpNavGraph(navController)
+                val navController = rememberNavController()
+                SetUpNavGraph(navController)
             }
         }
     } // End of onCreate()
