81dee2623db49446b1a1823235b736e223f8116d
ParkChaeSeong <97psh@naver.com>
2024-07-13 23:26:30 +0900

    [FEAT] 헤더 인터셉터 분리

--- MajuApp/app/src/main/java/com/app/majuapp/di/AppModule.kt
+++ MajuApp/app/src/main/java/com/app/majuapp/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.app.majuapp.di
 
+import com.app.majuapp.Application
 import com.app.majuapp.BuildConfig
 import com.app.majuapp.domain.api.CultureApi
 import com.app.majuapp.domain.api.LoginApi
@@ -18,26 +19,72 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 
 private const val TAG = "AppModule_창영"
 
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class HeaderInterceptorOkHttpClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WithoutHeaderInterceptorOkHttpClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class HeaderInterceptorRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class WithoutHeaderInterceptorRetrofit
+
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    class HeaderInterceptor @Inject constructor() : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+            val newRequest = request().newBuilder()
+                .addHeader("Authorization", Application.sharedPreferencesUtil.getUserAccessToken())
+                .build()
+            proceed(newRequest)
+        }
+    } // End of HeaderInterceptor class
 
     class AppInterceptor @Inject constructor() : Interceptor {
         override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
             val newRequest = request().newBuilder()
                 // 여기서 토큰이나 API Key넣으면 됨
-                .addHeader("Content-Type", "application/json")
-//                .addHeader("Authorization", "Client-ID ")
                 .build()
             proceed(newRequest)
         }
     } // End of AppInterceptor class
 
+    @HeaderInterceptorOkHttpClient
+    @Singleton
+    @Provides
+    fun providesHeaderInterceptorOkHttpClient(appInterceptor: HeaderInterceptor): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            logging.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            logging.level = HttpLoggingInterceptor.Level.NONE
+        }
+
+        return OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addNetworkInterceptor(HttpLoggingInterceptor())
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .addInterceptor(appInterceptor)
+            .build()
+    } // End of providesHeaderInterceptorOkHttpClient()
+
+    @WithoutHeaderInterceptorOkHttpClient
     @Singleton
     @Provides
     fun providesOkHttpClient(appInterceptor: AppInterceptor): OkHttpClient {
@@ -60,30 +107,43 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun providesApiService(retrofit: Retrofit): TestApi {
+    fun providesApiService(@WithoutHeaderInterceptorOkHttpClient retrofit: Retrofit): TestApi {
         return retrofit.create(TestApi::class.java)
     } // End of providesApiService()
 
+    @HeaderInterceptorRetrofit
     @Provides
     @Singleton
-    fun providesRetrofit(appInterceptor: AppInterceptor): Retrofit {
+    fun providesHeaderInterceptorRetrofit(@HeaderInterceptorOkHttpClient okHttpClient: OkHttpClient): Retrofit {
         val rxAdapter = RxJava3CallAdapterFactory.create()
 
         return Retrofit.Builder().baseUrl(Constants.API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create()).addCallAdapterFactory(rxAdapter)
-            .client(providesOkHttpClient(appInterceptor))
+            .client(okHttpClient)
+            .build()
+    } // End of providesHeaderInterceptorRetrofit()
+
+    @WithoutHeaderInterceptorRetrofit
+    @Provides
+    @Singleton
+    fun providesRetrofit(@WithoutHeaderInterceptorOkHttpClient okHttpClient: OkHttpClient): Retrofit {
+        val rxAdapter = RxJava3CallAdapterFactory.create()
+
+        return Retrofit.Builder().baseUrl(Constants.API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create()).addCallAdapterFactory(rxAdapter)
+            .client(okHttpClient)
             .build()
     } // End of providesRetrofit()
 
     @Provides
     @Singleton
-    fun providesLoginApi(retrofit: Retrofit): LoginApi {
+    fun providesLoginApi(@WithoutHeaderInterceptorRetrofit retrofit: Retrofit): LoginApi {
         return retrofit.create(LoginApi::class.java)
     } // End of providesLoginApi
 
     @Provides
     @Singleton
-    fun providesCultureApi(retrofit: Retrofit): CultureApi {
+    fun providesCultureApi(@HeaderInterceptorRetrofit retrofit: Retrofit): CultureApi {
         return retrofit.create(CultureApi::class.java)
     } // End of providesCultureApi
 
